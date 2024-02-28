57b01e21b23df63cc1f4dda68d460ef9d3aacb12
maiduong <duongmdph40323@gmail.com>
2024-02-28 21:56:34 +0700

    lesson 5

---
+++ lesson5/script.js
@@ -0,0 +1,6 @@
+let count=1;
+count='name';
+console.log(`check count = ${count}`);
+let names=['typescript','nextjs','javascript'];
+names.push(25);
+console.log(`Các phần tử trong names :[${names}]`);
---
+++ lesson5/script.ts
@@ -0,0 +1,5 @@
+let count2:string="Mai Duong";
+console.log(`check count 2 = ${count2}`);
+let names3=['typescript','nextjs','javascript',96];
+names3.push(12);
+console.log(`Các phần tử trong names 3:[${names3}]`);
