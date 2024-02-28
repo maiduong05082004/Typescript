e1dd058a1277cc918bec1fbd891147426fde575d
maiduong <duongmdph40323@gmail.com>
2024-02-28 22:24:28 +0700

    lisson 6

---
+++ lesson6/script.js
@@ -0,0 +1,7 @@
+let count=69;
+let score=6.98;
+count='name';
+console.log(`check count = ${count}`);
+let names=['typescript','nextjs','javascript'];
+names.push(25);
+console.log(`Các phần tử trong names :[${names}]`);
---
+++ lesson6/script.ts
@@ -0,0 +1,2 @@
+let test:number=6.9;
+test=999;
