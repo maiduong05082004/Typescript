27e3b9fd675d23878bf1c3dddec05cfc2b3a10fd
maiduong <duongmdph40323@gmail.com>
2024-02-29 12:05:36 +0700

    lesson 10

---
+++ lesson10/script.ts
@@ -0,0 +1,8 @@
+let skills:(string|number)[]=["JavaScript","Python",10,"Java"];
+skills.push("link");
+skills.push(43);
+//mảng trên không bị giới hạn bới các phần tử trong mảng khiến dữ liệu không được chặt chẽ
+
+let skills2:[string,number]=["JavaScript",10];
+let skills3:[boolean,string,number];
+skills3=[true,'karik',28];
