96747f2a922c8d782fe68102423f0df14b358f93
maiduong <duongmdph40323@gmail.com>
2024-02-29 23:23:40 +0700

    lesson 15

---
+++ lesson15/script.ts
@@ -0,0 +1,12 @@
+function addNumberOrString (a:number|string,b:number|string){
+    if(typeof a === 'number' && typeof b === 'number'){
+        return  a+b;
+    }
+    if(typeof a === 'string' && typeof b === 'string'){
+        return a.concat(b);
+    }
+    throw new Error('Parameters must be numbers or strings');
+}
+console.log(addNumberOrString(6,4));
+console.log(addNumberOrString("Ngọc"," Thư"));
+console.log(addNumberOrString("Ngọc",4));
