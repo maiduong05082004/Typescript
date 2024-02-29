73d05625cbebeef10d276b0ca554af91c847e798
maiduong <duongmdph40323@gmail.com>
2024-02-29 11:23:25 +0700

    lesson 8

---
+++ lesson8/script.js
@@ -0,0 +1,5 @@
+let mine = {};
+mine ={name:"karik"};
+mine.address="Lê Thánh Tông";
+//nếu làm như vậy thì sẽ không biết lỗi ở đâu nhưng thk js vẫn sẽ nhận dữ liệu và cho vào trong mảng mine
+console.log(mine);
---
+++ lesson8/script.ts
@@ -0,0 +1,19 @@
+
+let pro1:{
+    name: string,
+    age: number
+}={
+    name:"karik",
+    age:25
+}
+
+// hoặc có thể dùng interface như sau
+
+interface pesion{
+    name:string;
+    age:number;
+}
+let pro2:pesion= {
+    name:"karik",
+    age:30
+}
