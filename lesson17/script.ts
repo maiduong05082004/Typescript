7481723e4aa35e2aa728148670fd1b1fe3d830a2
maiduong <duongmdph40323@gmail.com>
2024-03-01 02:13:25 +0700

    lesson 17

---
+++ lesson17/script.ts
@@ -0,0 +1,16 @@
+let age:number=15;
+if(age<10){
+    console.log("Bạn đã đủ tuổi");
+}else if(age>10 && age<15){
+    console.log("Bạn chưa đủ tuổi");
+}else if(age>15&&age<20){
+    console.log("hôm nay thật buồn");
+}
+
+
+let name5:string="Mai Duong";
+if (name5){
+    console.log(`Bạn tên là ${name5}`);
+}else{
+    console.log(`Bạn không có tên`);
+}
