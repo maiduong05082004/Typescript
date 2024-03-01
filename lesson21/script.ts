bb340615805ab31a84247d991911831021dd36bf
maiduong <duongmdph40323@gmail.com>
2024-03-01 14:50:56 +0700

    lesson 21

---
+++ lesson21/script.ts
@@ -0,0 +1,13 @@
+let counter1=0;
+do {
+    console.log(`số ${counter1}`);
+    if(counter1==5) break;
+    counter1++;
+} while (counter1<5);
+
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 10);
+
