8acc77e4bdba4a6256ecdf209df33c7190769a31
maiduong <duongmdph40323@gmail.com>
2024-03-02 11:36:22 +0700

    lesson 26

--- lesson26/script.ts
+++ lesson26/script.ts
@@ -3,3 +3,18 @@ const sum5=(a:number,b:number,c?:number)=>{
     if(c) return a+b+c;
     return a+b;
 }
+function greet(name: string, greeting?: string) {
+    // Kiểm tra xem greeting có được cung cấp không
+    if (greeting) {
+      console.log(`${greeting}, ${name}!`);
+    } else {
+      console.log(`Hello, ${name}!`);
+    }
+  }
+  
+  // Gọi hàm với cả hai tham số
+  greet("Alice", "Hi");
+  
+  // Gọi hàm chỉ với tham số bắt buộc
+  greet("Bob");
+  
